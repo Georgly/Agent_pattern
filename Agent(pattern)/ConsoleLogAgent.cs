ba6cdf317781db9dbbfb1e51c219d95175b37d3c
Georgly <korsakov.georgy.s@gmail.com>
2017-05-04 13:53:58 +0300

    Add files via upload

---
+++ Agent(pattern)/ConsoleLogAgent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agent_pattern_
+{
+    class ConsoleLogAgent : AgentDecorator
+    {
+        public ConsoleLogAgent(AbstractAgent agent) : base(agent.fileName, agent)
+        { }
+
+        public override void ReadMessage()
+        {
+            agent.ReadMessage();
+        }
+
+        public override void ShowMessage()
+        {
+            agent.ShowMessage();
+            LogMessage();
+        }
+    }
+}
